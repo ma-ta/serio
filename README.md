f9402c697a85330480aca6cdd592c4ee62bf4c53
Martin Tábor <ma-ta@users.noreply.github.com>
2022-05-30 17:44:20 +0200

    verze 0.2.2
    VS Community 2022 v17.2.2
    .NET SDK 6.0.300

--- README.md
+++ README.md
@@ -3,15 +3,17 @@
 
 ### Použité technologie:
 
-- VS Community 2022 v17.0.0-preview.3,
-- .NET SDK 6.0.100-preview.7,
+- VS Community 2022 v17.2.2,
+- .NET SDK 6.0.300,
 - WPF
 
 ### Ke stažení:
 
-- [Serio 0.2.1 (win-x64)](//github.com/ma-ta/serio/tree/main/src/bin/Release/net6.0-windows10.0.19041.0/publish/Serio.exe)
-- [Běhové prostředí .NET 6.0](//dotnet.microsoft.com/download/dotnet/6.0)
+- [Serio 0.2.2 (win-x86)](//)
+- [Serio 0.2.2 (win-x64)](//)
+- [Serio 0.2.2 (win-arm64)](//)
+- [Běhové prostředí .NET](//dotnet.microsoft.com/download/dotnet/current/runtime)
 
 ### Screenshot:
 
-![Screenshot aplikace Serio v0.2.0](/serio.png)
+![Screenshot aplikace Serio v0.2](/serio_v0.2_win11.png)
--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -14,11 +14,11 @@ namespace Serio
     public partial class App : Application
     {
         // programové konstanty
-        public static string NAZEV = "Serio";
+        public static string NAZEV    = "Serio";
         public static string PODTITUL = "Sériový terminál";
-        public static string AUTOR = "Martin TÁBOR";
-        public static string LICENCE = "CC BY-SA 4.0";
-        public static string VERZE = "0.2.1  |  21.815";
-        public static string GITHUB = "github.com/ma-ta/serio";
+        public static string AUTOR    = "Martin TÁBOR";
+        public static string LICENCE  = "CC BY-SA 4.0";
+        public static string VERZE    = "0.2.2  |  22.E30";
+        public static string GITHUB   = "github.com/ma-ta/serio";
     }
 }
